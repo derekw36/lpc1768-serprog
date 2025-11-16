b7960e1a9da003f4ef3ef175703faa193a4adb3c
Derek Watson <derekw1996@gmail.com>
2025-11-16 18:11:20 -0500

    rewrite as class, inherit from USBSerial

--- main.cpp
+++ main.cpp
@@ -27,26 +27,95 @@
 #define MIN(a, b) MIN_MAX(a, b, <)
 #define MAX(a, b) MIN_MAX(a, b, >)
 
+// Peripheral setup
 BusOut leds(LED4, LED3, LED2, LED1);
 SPI spi(MBED_SPI0, use_gpio_ssel);
-USBSerial ser(false);
 
-int main(void)
+// Extend the USBSerial class to handle serprog commands
+class SerProg : private USBSerial
 {
-    printf("%s up and running!\n", SERPROG_NAME);
-    ser.connect();
+    SPI *spi;
+    uint8_t buf[4096];
 
-    while(1)
+    ssize_t ser_to_spi(size_t len)
     {
-        leds = 0x00;
-        ser.wait_ready();
-        int cmd = ser.getc();
+        ssize_t total = 0;
+        ssize_t rbytes = 0;
+        ssize_t wbytes = 0;
 
-        switch(cmd)
+        // transfer from ser to spi in chunks
+        leds = 0x01;
+        while(total < (ssize_t)len)
+        {
+            rbytes = read(buf, MIN(len, sizeof(buf)));
+            if(rbytes < 0)
+            {
+                break;
+            }
+
+            wbytes = spi->write(buf, rbytes, NULL, 0);
+            if(wbytes < 0)
+            {
+                break;
+            }
+
+            total += wbytes;
+            leds = (leds == 0x08) ? 0x01 : leds << 1;
+        }
+
+        return total;
+    }
+
+    ssize_t spi_to_ser(size_t len)
+    {
+        ssize_t total = 0;
+        ssize_t rbytes = 0;
+        ssize_t wbytes = 0;
+
+        // transfer from spi to ser in chunks
+        leds = 0x08;
+        while(total < (ssize_t)len)
+        {
+            rbytes = spi->write(NULL, 0, buf, MIN(len, sizeof(buf)));
+            if(rbytes < 0)
+            {
+                break;
+            }
+
+            wbytes = write(buf, rbytes);
+            if(wbytes < 0)
+            {
+                break;
+            }
+
+            total += wbytes;
+            leds = (leds == 0x01) ? 0x08 : leds >> 1;
+        }
+
+        return total;
+    }
+
+public:
+    SerProg(SPI *spi) : USBSerial(false), spi(spi)
+    {
+        // Configure SPI bus before connecting USB
+        this->spi->format(8, 0);
+        this->spi->set_default_write_value(0x00);
+
+        // Connect USB
+        connect();
+    }
+
+    int transact(void)
+    {
+        // Wait until serprog connects to us
+        wait_ready();
+
+        switch(_getc())
         {
             case S_CMD_NOP:
             {
-                ser.putc(S_ACK);
+                _putc(S_ACK);
                 break;
             }
 
@@ -54,8 +123,8 @@ int main(void)
             {
                 static const uint16_t ver = S_IFACE_VER;
 
-                ser.putc(S_ACK);
-                ser.write(&ver, 2);
+                _putc(S_ACK);
+                write(&ver, 2);
                 break;
             }
 
@@ -65,8 +134,8 @@ int main(void)
                 uint8_t map[32] = {0};
                 memcpy(map, &cmds, sizeof(cmds));
 
-                ser.putc(S_ACK);
-                ser.write(map, 32);
+                _putc(S_ACK);
+                write(map, 32);
                 break;
             }
 
@@ -75,8 +144,8 @@ int main(void)
                 char name[16] = {0};
                 strncpy(name, SERPROG_NAME, sizeof(name));
 
-                ser.putc(S_ACK);
-                ser.write(name, 16);
+                _putc(S_ACK);
+                write(name, 16);
                 break;
             }
 
@@ -84,117 +153,98 @@ int main(void)
             {
                 static const uint16_t sz = 0xFFFF;
 
-                ser.putc(S_ACK);
-                ser.write(&sz, 2);
+                _putc(S_ACK);
+                write(&sz, 2);
                 break;
             }
 
             case S_CMD_Q_BUSTYPE:
             {
-                ser.putc(S_ACK);
-                ser.putc(BUS_SPI);
+                _putc(S_ACK);
+                _putc(BUS_SPI);
                 break;
             }
 
             case S_CMD_SYNCNOP:
             {
-                ser.putc(S_NAK);
-                ser.putc(S_ACK);
+                _putc(S_NAK);
+                _putc(S_ACK);
                 break;
             }
 
             case S_CMD_S_BUSTYPE:
             {
-                int bus = ser.getc();
+                int bus = _getc();
 
                 if(bus == BUS_SPI)
                 {
                     // We are always SPI! Just ACK it
-                    ser.putc(S_ACK);
+                    _putc(S_ACK);
                 }
                 else
                 {
-                    ser.putc(S_NAK);
+                    _putc(S_NAK);
                 }
                 break;
             }
 
             case S_CMD_O_SPIOP:
             {
-                static uint8_t buf[4096];
-                ssize_t bytes = 0;
-                size_t total = 0;
                 uint32_t slen = 0;
                 uint32_t rlen = 0;
 
-                ser.read(&slen, 3);
-                ser.read(&rlen, 3);
+                read(&slen, 3);
+                read(&rlen, 3);
 
-                spi.select();
+                spi->select();
 
-                // transfer from ser to spi in chunks
-                leds = 0x01;
-                while(total < slen)
-                {
-                    leds = (leds == 0x08) ? 0x01 : leds << 1;
-                    bytes = ser.read(buf, MIN(slen, sizeof(buf)));
-                    if(bytes > 0)
-                    {
-                        spi.write(buf, bytes, NULL, 0);
-                        total += bytes;
-                    }
-                    else if(bytes < 0)
-                    {
-                        break;
-                    }
-                }
+                ser_to_spi(slen);
+                _putc(S_ACK);
+                spi_to_ser(rlen);
 
-                // transfer from spi to ser in chunks
-                bytes = 0, total = 0;
-                leds = 0x08;
-                ser.putc(S_ACK);
-                while(total < rlen)
-                {
-                    leds = (leds == 0x01) ? 0x08 : leds >> 1;
-                    bytes = spi.write(NULL, 0, buf, MIN(rlen, sizeof(buf)));
-                    if(bytes > 0)
-                    {
-                        if(ser.write(buf, bytes) < 0)
-                        {
-                            break;
-                        }
-                        total += bytes;
-                    }
-                }
-
-                spi.deselect();
+                spi->deselect();
                 break;
             }
 
             case S_CMD_S_SPI_FREQ:
             {
                 uint32_t freq = 0;
-                ser.read(&freq, 4);
+                read(&freq, 4);
 
                 if(freq != 0)
                 {
-                    spi.frequency((int)freq);
+                    spi->frequency((int)freq);
 
-                    ser.putc(S_ACK);
-                    ser.write(&freq, 4);
+                    _putc(S_ACK);
+                    write(&freq, 4);
                 }
                 else
                 {
-                    ser.putc(S_NAK);
+                    _putc(S_NAK);
                 }
                 break;
             }
 
             default:
             {
-                ser.putc(S_NAK);
+                _putc(S_NAK);
                 break;
             }
         }
+
+        return 0;
     }
+};
+
+int main(void)
+{
+    SerProg sp(&spi);
+
+    printf("%s up and running!\n", SERPROG_NAME);
+    while(1)
+    {
+        sp.transact();
+    }
+
+    return 0;
 }
